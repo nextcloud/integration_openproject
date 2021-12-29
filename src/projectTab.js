73b75dda6e3e41d1ab8d79ee9200142ba9788a90
Artur Neumann <artur@jankaritech.com>
2021-12-29 17:16:09 +0545

    fix license header to make eslint happy

--- src/projectTab.js
+++ src/projectTab.js
@@ -3,7 +3,7 @@
  *
  * @author Kiran Parajuli <kiran@jankaritech.com>
  *
- * @license GNU Affero General Public License v3.0 or later
+ * @license AGPL-3.0-or-later
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
