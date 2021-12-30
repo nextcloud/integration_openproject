b84c4a5fb24a2e4e7415b9c2f83ce9881fcc02a4
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-12-30 13:37:10 +0545

    Used flow svg for empty content view

--- src/components/tab/EmptyContent.vue
+++ src/components/tab/EmptyContent.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="empty-projects">
 		<div class="empty-icon">
-			<div class="icon" />
+			<img :src="flowSvg" alt="flow">
 		</div>
-		<div class="title">
-			No OpenProject links yet
+		<div class="title text-center">
+			{{ t('integration_openproject', "No OpenProject links yet") }}
 		</div>
-		<div class="subtitle">
-			To add a link, use the search bar above to find the desired work package.
+		<div class="subtitle text-center">
+			{{ t('integration_openproject', "To add a link, use the search bar above to find the desired work package.") }}
 		</div>
 	</div>
 </template>
@@ -15,33 +15,43 @@
 <script>
 export default {
 	name: 'EmptyContent',
+	computed: {
+		flowSvg() {
+			return require('../../../img/flow.svg')
+		},
+	},
 }
 </script>
 
 <style scoped lang="scss">
 .empty-projects {
+	.text-center {
+		text-align: center;
+	}
 	.empty-icon {
 		padding-top: 10vh;
 		padding-bottom: 1vh;
 		display: flex;
 		align-content: center;
 		justify-content: center;
-		.icon {
+		.icon img {
 			height: 50px;
 			width: 50px;
-			background: brown;
 		}
 	}
 	.title {
-		text-align: center;
 		font-size: 1.2rem;
+		line-height: 1.4rem;
 		font-weight: 600;
+		padding-top: 4px;
+		color: #333333;
 	}
 	.subtitle {
-		text-align: center;
 		font-size: .875rem;
 		font-weight: 400;
 		color: #6d6d6d;
+		line-height: 1rem;
+		padding: 8px 0;
 	}
 }
 </style>
