921867760a56a8488283ae6100cc6dd7e3047944
Parajuli Kiran <kiranparajuli589@gmail.com>
2022-01-19 21:06:10 +0545

    used the open-project icon for the sidebar tab
    Signed-off-by: Parajuli Kiran <kiranparajuli589@gmail.com>

--- src/projectTab.js
+++ src/projectTab.js
@@ -36,7 +36,7 @@ let TabInstance = null
 const projectTab = new OCA.Files.Sidebar.Tab({
 	id: 'open-project',
 	name: t('integration_openproject', 'OpenProject'),
-	icon: 'icon-projects',
+	icon: 'icon-open-project',
 
 	async mount(el, fileInfo, context) {
 		if (TabInstance) {
