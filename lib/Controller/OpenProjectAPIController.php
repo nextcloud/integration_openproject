9957bfd2a6c1b4bb7326ea53d3789fb16d86bfa6
Artur Neumann <artur@jankaritech.com>
2022-03-14 09:30:50 +0545

    improve error message
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- lib/Controller/OpenProjectAPIController.php
+++ lib/Controller/OpenProjectAPIController.php
@@ -206,7 +206,7 @@ class OpenProjectAPIController extends Controller {
 		} catch (OpenprojectErrorException $e) {
 			return new DataResponse($e->getMessage(), Http::STATUS_BAD_REQUEST);
 		} catch (NotPermittedException | NotFoundException $e) {
-			return new DataResponse('fileid not found', Http::STATUS_NOT_FOUND);
+			return new DataResponse('file not found', Http::STATUS_NOT_FOUND);
 		} catch (Exception $e) {
 			return new DataResponse($e->getMessage(), Http::STATUS_INTERNAL_SERVER_ERROR);
 		}
