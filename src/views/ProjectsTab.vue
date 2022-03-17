3987cbfa5caa9ac599c291593e65d8a91b9363c3
Artur Neumann <artur@jankaritech.com>
2022-03-17 16:14:27 +0545

    filter out already linked files from search
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- src/views/ProjectsTab.vue
+++ src/views/ProjectsTab.vue
@@ -24,6 +24,7 @@
 	<div class="projects">
 		<SearchInput v-if="!!requestUrl"
 			:file-info="fileInfo"
+			:linked-work-packages="workpackages"
 			@saved="onSaved" />
 		<div v-if="isLoading" class="icon-loading" />
 		<div v-else-if="workpackages.length > 0" id="openproject-linked-workpackages">
