ff3af5dee80fd156a23977e715194b9433e5efa9
Swikriti Tripathi <swikriti808@gmail.com>
2022-05-30 14:14:37 +0545

    Handle error while unlinking work packages
    Signed-off-by: Swikriti Tripathi <swikriti808@gmail.com>

--- src/views/ProjectsTab.vue
+++ src/views/ProjectsTab.vue
@@ -185,13 +185,15 @@ export default {
 			} catch (e) {
 				response = e.response
 			}
-			this.checkForErrorCode(response.status)
 			if (response.status === 200) {
 				const id = this.processLink(response.data, fileId)
 				const url = generateUrl('/apps/integration_openproject/file-links/' + id)
 				response = await axios.delete(url)
-				return response
+			} else {
+				this.checkForErrorCode(response.status)
+				throw new Error()
 			}
+			return response
 		},
 		processLink(data, fileId) {
 			let linkId
