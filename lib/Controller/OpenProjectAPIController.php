ba60c44c3d07266e8ef5e6e4ffa84ab6f76b4352
Artur Neumann <artur@jankaritech.com>
2022-06-17 11:33:00 +0545

    move generating OP OAuth URI to the backend
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- lib/Controller/OpenProjectAPIController.php
+++ lib/Controller/OpenProjectAPIController.php
@@ -26,6 +26,7 @@ use OCP\AppFramework\Controller;
 
 use OCA\OpenProject\Service\OpenProjectAPIService;
 use OCA\OpenProject\AppInfo\Application;
+use OCP\IURLGenerator;
 
 class OpenProjectAPIController extends Controller {
 
@@ -47,16 +48,29 @@ class OpenProjectAPIController extends Controller {
 	 */
 	private $openprojectUrl;
 
+	/**
+	 * @var IConfig
+	 */
+	private $config;
+
+	/**
+	 * @var IURLGenerator
+	 */
+	private $urlGenerator;
+
 	public function __construct(string $appName,
 								IRequest $request,
 								IConfig $config,
 								OpenProjectAPIService $openprojectAPIService,
+								IURLGenerator $urlGenerator,
 								?string $userId) {
 		parent::__construct($appName, $request);
 		$this->openprojectAPIService = $openprojectAPIService;
 		$this->userId = $userId;
 		$this->accessToken = $config->getUserValue($userId, Application::APP_ID, 'token');
 		$this->openprojectUrl = $config->getAppValue(Application::APP_ID, 'oauth_instance_url');
+		$this->config = $config;
+		$this->urlGenerator = $urlGenerator;
 	}
 
 	/**
@@ -321,5 +335,45 @@ class OpenProjectAPIController extends Controller {
 			return new DataResponse(false);
 		}
 		return new DataResponse(false);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 *
+	 * @return DataResponse
+	 */
+	public function getOpenProjectOauthURL(): DataResponse {
+		$url = $this->openprojectAPIService::getOpenProjectOauthURL(
+			$this->config, $this->urlGenerator
+		);
+		$oauthState = bin2hex(random_bytes(5));
+		$this->config->setUserValue(
+			$this->userId,
+			Application::APP_ID,
+			'oauth_state',
+			$oauthState
+		);
+		$randomString = bin2hex(random_bytes(64));
+		$codeVerifier = $this->base64UrlEncode(pack('H*', $randomString));
+		$this->config->setUserValue(
+			$this->userId,
+			Application::APP_ID,
+			'code_verifier',
+			$codeVerifier
+		);
+		$hash = hash('sha256', $codeVerifier);
+		$codeChallenge = $this->base64UrlEncode(pack('H*', $hash));
+		$url = $url . '&state=' .$oauthState .
+				 '&code_challenge=' . $codeChallenge .
+				'&code_challenge_method=S256';
+
+		return new DataResponse($url);
+	}
+
+	private function base64UrlEncode($plainText) {
+		$base64 = base64_encode($plainText);
+		$base64 = trim($base64, "=");
+		$base64url = strtr($base64, '+/', '-_');
+		return ($base64url);
 	}
 }
--- src/components/OAuthConnectButton.vue
+++ src/components/OAuthConnectButton.vue
@@ -33,47 +33,15 @@ export default {
 	},
 
 	methods: {
-		generateRandomString(length) {
-			let text = ''
-			const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~'
-			for (let i = 0; i < length; i++) {
-				text += possible.charAt(Math.floor(Math.random() * possible.length))
-			}
-			return text
-		},
-		async digest(text) {
-			return await crypto.subtle.digest('SHA-256', new TextEncoder().encode(text))
-		},
-		async generateCodeChallenge(codeVerifier) {
-			const digest = await this.digest(codeVerifier)
-			return btoa(String.fromCharCode(...new Uint8Array(digest)))
-				.replace(/=/g, '')
-				.replace(/\+/g, '-')
-				.replace(/\//g, '_')
-		},
 		async onOAuthClick() {
-			const oauthState = this.generateRandomString(10)
-			const codeVerifier = this.generateRandomString(128)
-			const codeChallenge = await this.generateCodeChallenge(codeVerifier)
-			const requestUrl = this.requestUrl
-				+ '&state=' + encodeURIComponent(oauthState)
-				+ '&code_challenge=' + codeChallenge
-				+ '&code_challenge_method=S256'
-
-			const req = {
-				values: {
-					oauth_state: oauthState,
-					code_verifier: codeVerifier,
-				},
-			}
-			const url = generateUrl('/apps/integration_openproject/config')
-			axios.put(url, req)
-				.then(() => {
-					window.location.replace(requestUrl)
+			const url = generateUrl('/apps/integration_openproject/op-oauth-url')
+			axios.get(url)
+				.then((result) => {
+					window.location.replace(result.data)
 				})
 				.catch((error) => {
 					showError(
-						t('integration_openproject', 'Failed to save OpenProject OAuth state')
+						t('integration_openproject', 'Failed to redirect to OpenProject')
 						+ ': ' + error.message
 					)
 				})
