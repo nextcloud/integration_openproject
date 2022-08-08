54ec16b4355cd65aa8b40a3e59bab59cab02f44f
Artur Neumann <artur@jankaritech.com>
2022-08-08 12:07:56 +0545

    dedublicate code
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- lib/Controller/ConfigController.php
+++ lib/Controller/ConfigController.php
@@ -236,15 +236,12 @@ class ConfigController extends Controller {
 		}
 
 		if ($clientID && $validClientSecret && $validCodeVerifier && $configState !== '' && $configState === $state) {
-			$redirectUri = $this->urlGenerator->getAbsoluteURL(
-				'/index.php/apps/' . Application::APP_ID . '/oauth-redirect'
-			);
 			$openprojectUrl = $this->config->getAppValue(Application::APP_ID, 'oauth_instance_url');
 			$result = $this->openprojectAPIService->requestOAuthAccessToken($openprojectUrl, [
 				'client_id' => $clientID,
 				'client_secret' => $clientSecret,
 				'code' => $code,
-				'redirect_uri' => $redirectUri,
+				'redirect_uri' => openprojectAPIService::getOauthRedirectUrl($this->urlGenerator),
 				'grant_type' => 'authorization_code',
 				'code_verifier' => $codeVerifier
 			], 'POST');
