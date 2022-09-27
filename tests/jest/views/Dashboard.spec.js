0f7225e2dfd6ea783dc1bf542346eac4df2b5929
Artur Neumann <artur@jankaritech.com>
2022-09-27 11:56:37 +0545

    test if error message is displayed
    Signed-off-by: Artur Neumann <artur@jankaritech.com>

--- tests/jest/views/Dashboard.spec.js
+++ tests/jest/views/Dashboard.spec.js
@@ -8,6 +8,7 @@ import { STATE } from '../../../src/utils'
 import notificationsResponse from '../fixtures/notificationsResponse.json'
 import workPackagesSearchResponse
 	from '../fixtures/workPackagesSearchResponse.json'
+import * as dialogs from "@nextcloud/dialogs";
 
 jest.mock('@nextcloud/axios')
 jest.mock('@nextcloud/l10n', () => ({
@@ -93,76 +94,103 @@ describe('Dashboard.vue', () => {
 		])
 		axiosSpy.mockRestore()
 	})
-	it('should mark notifications as read', async () => {
-		wrapper = mount(
-			Dashboard,
-			{
-				localVue,
-				attachTo: document.body,
-				mocks: {
-					t: (app, msg) => msg,
-					generateUrl() {
-						return '/'
-					},
-				},
-				stubs: {
-					Avatar: true,
-				},
-				propsData: {
-					title: 'dashboard',
-				},
-			})
-		const axiosSpyGet = jest.spyOn(axios, 'get')
-			.mockImplementation(() => Promise.resolve({
-				data: [
-					{
-						_type: 'Notification',
-						id: 47,
-						readIAN: false,
-						reason: 'assigned',
-						createdAt: '2022-08-17T10:27:01Z',
-						updatedAt: '2022-08-17T10:28:12Z',
-						_links: {
-							self: {
-								href: '/api/v3/notifications/47',
-							},
-							readIAN: {
-								href: '/api/v3/notifications/47/read_ian',
-								method: 'post',
-							},
-							actor: {
-								href: '/api/v3/users/8',
-								title: 'Admin de DEV user',
-							},
-							project: {
-								href: '/api/v3/projects/4',
-								title: 'Dev-large',
-							},
-							activity: {
-								href: '/api/v3/activities/261',
-							},
-							resource: {
-								href: '/api/v3/work_packages/36',
-								title: 'write a software',
-							},
+	describe('mark as read', () => {
+		let axiosSpyGet
+		beforeEach(() => {
+			wrapper = mount(
+				Dashboard,
+				{
+					localVue,
+					attachTo: document.body,
+					mocks: {
+						t: (app, msg) => msg,
+						generateUrl() {
+							return '/'
 						},
 					},
-				],
-			}))
-		const axiosSpyDelete = jest.spyOn(axios, 'delete')
-			.mockImplementationOnce(() => Promise.resolve({})
+					stubs: {
+						Avatar: true,
+					},
+					propsData: {
+						title: 'dashboard',
+					},
+				})
+			axiosSpyGet = jest.spyOn(axios, 'get')
+				.mockImplementation(() => Promise.resolve({
+					data: [
+						{
+							_type: 'Notification',
+							id: 47,
+							readIAN: false,
+							reason: 'assigned',
+							createdAt: '2022-08-17T10:27:01Z',
+							updatedAt: '2022-08-17T10:28:12Z',
+							_links: {
+								self: {
+									href: '/api/v3/notifications/47',
+								},
+								readIAN: {
+									href: '/api/v3/notifications/47/read_ian',
+									method: 'post',
+								},
+								actor: {
+									href: '/api/v3/users/8',
+									title: 'Admin de DEV user',
+								},
+								project: {
+									href: '/api/v3/projects/4',
+									title: 'Dev-large',
+								},
+								activity: {
+									href: '/api/v3/activities/261',
+								},
+								resource: {
+									href: '/api/v3/work_packages/36',
+									title: 'write a software',
+								},
+							},
+						},
+					],
+				}))
+		})
+		afterEach(() => {
+			axiosSpyGet.mockRestore()
+		})
+		it('should mark notifications as read', async () => {
+			const axiosSpyDelete = jest.spyOn(axios, 'delete')
+				.mockImplementationOnce(() => Promise.resolve({})
+				)
+			dialogs.showSuccess.mockImplementationOnce()
+			await wrapper.vm.fetchNotifications()
+			await localVue.nextTick()
+			await wrapper.find(dashboardTriggerButtonSelector).trigger('click')
+			await localVue.nextTick()
+			await wrapper.find(markAsReadButtonSelector).trigger('click')
+			await localVue.nextTick()
+			expect(axiosSpyDelete).toHaveBeenCalledWith(
+				'http://localhost/apps/integration_openproject/work-packages/36/notifications'
 			)
-		await wrapper.vm.fetchNotifications()
-		await localVue.nextTick()
-		await wrapper.find(dashboardTriggerButtonSelector).trigger('click')
-		await localVue.nextTick()
-		await wrapper.find(markAsReadButtonSelector).trigger('click')
-		await localVue.nextTick()
-		expect(axiosSpyDelete).toHaveBeenCalledWith(
-			'http://localhost/apps/integration_openproject/work-packages/36/notifications'
-		)
-		wrapper.destroy()
-		axiosSpyGet.mockRestore()
-		axiosSpyDelete.mockRestore()
+			expect(dialogs.showSuccess).toHaveBeenCalledWith(
+				'Notifications associated with Work package marked as read'
+			)
+			wrapper.destroy()
+			axiosSpyDelete.mockRestore()
+		})
+		it('should show an error message if marking as read failed', async () => {
+			const axiosSpyDelete = jest.spyOn(axios, 'delete')
+				.mockRejectedValueOnce()
+			dialogs.showError.mockImplementationOnce()
+			await wrapper.vm.fetchNotifications()
+			await localVue.nextTick()
+			await wrapper.find(dashboardTriggerButtonSelector).trigger('click')
+			await localVue.nextTick()
+			await wrapper.find(markAsReadButtonSelector).trigger('click')
+			await localVue.nextTick()
+			expect(dialogs.showError).toHaveBeenCalledWith(
+				'Failed to mark notifications as read'
+			)
+			wrapper.destroy()
+			axiosSpyDelete.mockRestore()
+		})
 	})
 })
