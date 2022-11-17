66f8570639ae8b834c2d701ef3fc019626a91f43
Swikriti Tripathi <swikriti808@gmail.com>
2022-11-17 14:17:56 +0545

    test
    Signed-off-by: Swikriti Tripathi <swikriti808@gmail.com>

--- tests/e2e/pageObjects/NextcloudLoginPage.js
+++ tests/e2e/pageObjects/NextcloudLoginPage.js
@@ -5,10 +5,10 @@ class NextcloudLoginPage {
 		this.usernameSelector = '#user'
 		this.passwordSelector = '#password'
 		this.submitButtonSelector = '//button[@type="submit"]'
-		this.hintSkipButtonSelector = '//div[contains(@class,"enjoyhint_btn-transparent") and (text() = "Skip")]'
 	}
 
 	async userLogsInNextcloud(username, password) {
+		console.log(config.baseUrlNC)
 		await pageNC.goto(config.baseUrlNC)
 		await pageNC.fill(this.usernameSelector, username)
 		await pageNC.fill(this.passwordSelector,password)
