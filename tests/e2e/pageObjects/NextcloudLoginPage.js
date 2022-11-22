89975a1eb3c114ee5f0b13a04f02a5bf74c7d708
Swikriti Tripathi <swikriti808@gmail.com>
2022-11-22 16:09:22 +0545

    make app

--- tests/e2e/pageObjects/NextcloudLoginPage.js
+++ tests/e2e/pageObjects/NextcloudLoginPage.js
@@ -5,7 +5,9 @@ class NextcloudLoginPage {
 		this.usernameSelector = '#user'
 		this.passwordSelector = '#password'
 		this.submitButtonSelector = '//button[@type="submit"]'
+		this.recommendedAppsSelector = '//div[@class="guest-box"]'
 		this.skipReccomendedAppsSelector = '//span[contains(@class,"button-vue__text") and contains(text(),"Skip")]'
+		this.backToNextcloudButtonSelector = '//a[contains(text(),"Back to Nextcloud")]'
 	}
 
 	async userLogsInNextcloud(username, password) {
@@ -13,8 +15,9 @@ class NextcloudLoginPage {
 		await pageNC.fill(this.usernameSelector, username)
 		await pageNC.fill(this.passwordSelector,password)
 		await pageNC.click(this.submitButtonSelector)
-		await pageNC.waitForSelector(this.skipReccomendedAppsSelector)
+		await pageNC.waitForSelector(this.recommendedAppsSelector)
 		await pageNC.click(this.skipReccomendedAppsSelector)
+		await pageNC.click(this.backToNextcloudButtonSelector)
 	}
 }
 module.exports = { NextcloudLoginPage };
