ddb7f1b63f49946b549ec3c6c993c1cd5ee6aaeb
Swikriti Tripathi <swikriti808@gmail.com>
2022-11-24 15:43:18 +0545

    test with another page

--- tests/e2e/pageObjects/NextcloudAdminPage.js
+++ tests/e2e/pageObjects/NextcloudAdminPage.js
@@ -24,7 +24,7 @@ class NextcloudAdminPage {
 	}
 
 	async adminNavigatesToAdminOPTab() {
-		await pageNC.goto(config.baseUrlNC + '/index.php/settings/user/openprojectopenproject')
+		await pageNC.goto(config.baseUrlNC + '/index.php/settings/user/security')
 		await pageNC.screenshot({ path: 'report/screenshot.png', fullPage: true });
 		await pageNC.waitForSelector('#openproject_prefs',10000)
 		// await pageNC.waitForTimeout(10000)
