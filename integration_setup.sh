20d702714cca54639fcfa9962da1562ba18a107d
sagargurung1001@gmail.com <sagargurung1001@gmail.com>
2023-01-04 15:35:36 +0545

    Address review for the script
    Signed-off-by: sagargurung1001@gmail.com <sagargurung1001@gmail.com>

--- integration_setup.sh
+++ integration_setup.sh
@@ -32,7 +32,7 @@ then
 	exit 1
 fi
 
-# These URLs is are just to check if the nextcloud and openproject instances have been started or not before running the script
+# These URLs are just to check if the nextcloud and openproject instances have been started or not before running the script
 NEXTCLOUD_HOST_STATE=$(curl -s -X GET ${NEXTCLOUD_HOST}/cron.php)
 OPENPROJECT_HOST_STATE=$(curl -s -X GET ${OPENPROJECT_HOST}/api/v3/configuration | jq -r "._type")
 OPENPROJECT_BASEURL_FOR_STORAGE=${OPENPROJECT_HOST}/api/v3/storages
@@ -50,7 +50,7 @@ elif [[ "$NEXTCLOUD_HOST_STATE" != *"success"* ]]
 fi
 
 # api call to get openproject_client_id and openproject_client_secret
-CREATE_STORAGE_RESPONSE=$(curl -s -X POST -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} \
+create_storage_response=$(curl -s -X POST -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} \
                             ${OPENPROJECT_BASEURL_FOR_STORAGE} \
                             -H 'accept: application/hal+json' \
                             -H 'Content-Type: application/json' \
@@ -68,11 +68,11 @@ CREATE_STORAGE_RESPONSE=$(curl -s -X POST -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASS
                           }')
 
 # check for errors
-response_type=$(echo $CREATE_STORAGE_RESPONSE | jq -r "._type")
+response_type=$(echo $create_storage_response | jq -r "._type")
 if [[ ${response_type} == "Error" ]]; then
-	error_message=$(echo $CREATE_STORAGE_RESPONSE | jq -r ".message")
+	error_message=$(echo $create_storage_response | jq -r ".message")
 	if [[ ${error_message} == "Multiple field constraints have been violated." ]]; then
-		violated_error_messages=$(echo $CREATE_STORAGE_RESPONSE | jq -r "._embedded.errors[].message")
+		violated_error_messages=$(echo $create_storage_response | jq -r "._embedded.errors[].message")
 		log_error "'${NEXTCLOUD_HOST}' ${violated_error_messages}"
 		log_info "Try deleting the file storage in openproject and integrate again !!"
 		exit 1
@@ -87,12 +87,12 @@ if [[ ${response_type} == "Error" ]]; then
 fi
 
 # required information from the above response
-storage_id=$(echo $CREATE_STORAGE_RESPONSE | jq -e '.id')
-openproject_client_id=$(echo $CREATE_STORAGE_RESPONSE | jq -e '._embedded.oauthApplication.clientId')
-openproject_client_secret=$(echo $CREATE_STORAGE_RESPONSE | jq -e '._embedded.oauthApplication.clientSecret')
+storage_id=$(echo $create_storage_response | jq -e '.id')
+openproject_client_id=$(echo $create_storage_response | jq -e '._embedded.oauthApplication.clientId')
+openproject_client_secret=$(echo $create_storage_response | jq -e '._embedded.oauthApplication.clientSecret')
 
 if [ ${storage_id} == null ] || [ ${openproject_client_id} == null ] || [ ${openproject_client_secret} == null ]; then
-	echo "${CREATE_STORAGE_RESPONSE}" | jq
+	echo "${create_storage_response}" | jq
 	log_error "Response does not contain storage_id (id) or openproject_client_id (clientId) or openproject_client_secret (clientSecret)"
 	log_error "Integration failed :( !!"
 	exit 1
@@ -101,7 +101,7 @@ fi
 log_info "File Storage creation ${OPENPROJECT_STORAGE_NAME} successful ...."
 
 # api call to set the  openproject_client_id and openproject_client_secret to nextcloud and also get nextcloud_client_id and nextcloud_client_secret
-NEXTCLOUD_INFORMATION_RESPONSE=$(curl -s -XPOST -u${NC_ADMIN_USERNAME}:${NC_ADMIN_PASSWORD} ${INTEGRATION_URL_FOR_SETUP} \
+nextcloud_information_response=$(curl -s -XPOST -u${NC_ADMIN_USERNAME}:${NC_ADMIN_PASSWORD} ${INTEGRATION_URL_FOR_SETUP} \
 						   -d '{
 						   "values":{
 								   "openproject_instance_url":"'${OPENPROJECT_HOST}'",
@@ -114,11 +114,11 @@ NEXTCLOUD_INFORMATION_RESPONSE=$(curl -s -XPOST -u${NC_ADMIN_USERNAME}:${NC_ADMI
 						   -H 'Content-Type: application/json')
 
 # required information from the above response
-nextcloud_client_id=$(echo $NEXTCLOUD_INFORMATION_RESPONSE | jq -e ".nextcloud_client_id")
-nextcloud_client_secret=$(echo $NEXTCLOUD_INFORMATION_RESPONSE | jq -e ".nextcloud_client_secret")
+nextcloud_client_id=$(echo $nextcloud_information_response | jq -e ".nextcloud_client_id")
+nextcloud_client_secret=$(echo $nextcloud_information_response | jq -e ".nextcloud_client_secret")
 
 if [ ${nextcloud_client_id} == null ] || [ ${nextcloud_client_secret} == null ]; then
-	echo "${NEXTCLOUD_INFORMATION_RESPONSE}" | jq
+	echo "${nextcloud_information_response}" | jq
 	log_error "Response does not contain nextcloud_client_id (nextcloud_client_id) or nextcloud_client_secret(nextcloud_client_secret)"
 	log_error "Integration failed :( !!"
 	exit 1
@@ -127,7 +127,7 @@ fi
 log_info "Setting up Open project for Nextcloud successful ..."
 
 # api call to set the nextcloud_client_id and nextcloud_client_secret to openproject files storage
-SET_NC_TO_STORAGE_RESPONSE=$(curl -s -X POST -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} \
+set_nextcloud_to_storage_response=$(curl -s -X POST -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} \
                                   ${OPENPROJECT_BASEURL_FOR_STORAGE}/${storage_id}/oauth_client_credentials \
                                   -H 'accept: application/hal+json' \
                                   -H 'Content-Type: application/json' \
@@ -141,9 +141,9 @@ log_info "Setting up Nextcloud information on OpenProject successful ..."
 
 # if there is no error from the last api call then the integration can be declared successful
 
-response_type=$(echo $SET_NC_TO_STORAGE_RESPONSE | jq -r "._type")
+response_type=$(echo $set_nextcloud_to_storage_response | jq -r "._type")
 if [ ${nextcloud_client_id} == "Error" ]; then
-	error_message=$(echo $SET_NC_TO_STORAGE_RESPONSE | jq -r ".message")
+	error_message=$(echo $set_nextcloud_to_storage_response | jq -r ".message")
 	log_error "${error_message}"
 	log_error "Integration failed :( !!"
 	exit 1
