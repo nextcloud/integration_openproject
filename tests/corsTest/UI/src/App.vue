5daa6f41567dbd97902355a6bebd982cc6992144
Swikriti Tripathi <swikriti808@gmail.com>
2023-01-17 17:30:17 +0545

    Cors test app

---
+++ tests/corsTest/UI/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
