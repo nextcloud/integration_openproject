48f12be2a3f1227cbaa4a6ab42ea34cb431fe788
Artur Neumann <artur@jankaritech.com>
2023-01-27 15:38:45 +0545

    seperate API tests and run them on different DB (#323)

--- tests/acceptance/features/bootstrap/FeatureContext.php
+++ tests/acceptance/features/bootstrap/FeatureContext.php
@@ -92,7 +92,12 @@ class FeatureContext implements Context {
 			'/cloud/users', 'POST', $this->getAdminUsername(), $userAttributes
 		);
 		$this->theHttpStatusCodeShouldBe(200);
-		$this->userHasDeletedFile($user, "welcome.txt");
+		$this->response = $this->makeDavRequest(
+			$user,
+			$this->regularUserPassword,
+			"DELETE",
+			"welcome.txt"
+		);
 	}
 
 	/**
@@ -818,6 +823,8 @@ class FeatureContext implements Context {
 	 * @return void
 	 */
 	public function before(BeforeScenarioScope $scope):void {
+		setlocale(LC_ALL, 'C.utf8');
+
 		// Get the environment
 		$environment = $scope->getEnvironment();
 
