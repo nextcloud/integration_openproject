45a65275b5e2182be9a74c64f74ddf3fd727fd7e
Julien Veyssier <julien-nc@posteo.net>
2023-04-27 11:20:54 +0200

    start implementing a reference provider and a reference widget that are used on NC >= 26 only
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>
    Signed-off-by: Swikriti Tripathi <swikriti808@gmail.com>

---
+++ lib/Reference/WorkPackageReferenceProvider.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2023 Julien Veyssier <julien-nc@posteo.net>
+ *
+ * @author Julien Veyssier <julien-nc@posteo.net>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\OpenProject\Reference;
+
+use OCA\OpenProject\Service\OpenProjectAPIService;
+use OCP\Collaboration\Reference\ADiscoverableReferenceProvider;
+use OCP\Collaboration\Reference\ISearchableReferenceProvider;
+use OCP\Collaboration\Reference\Reference;
+use OC\Collaboration\Reference\ReferenceManager;
+use OCA\OpenProject\AppInfo\Application;
+use OCP\Collaboration\Reference\IReference;
+use OCP\IConfig;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+
+class WorkPackageReferenceProvider extends ADiscoverableReferenceProvider implements ISearchableReferenceProvider {
+	private const RICH_OBJECT_TYPE = Application::APP_ID . '_work_package';
+
+	// as we know we are on NC >= 26, we can use Php 8 syntax for class attributes
+	public function __construct(private IConfig $config,
+								private IL10N $l10n,
+								private IURLGenerator $urlGenerator,
+								private ReferenceManager $referenceManager,
+								private OpenProjectAPIService $openProjectAPIService,
+								private ?string $userId) {
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getId(): string {
+		return 'openproject-work-package-ref';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getTitle(): string {
+		return $this->l10n->t('OpenProject work packages');
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getOrder(): int {
+		return 10;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIconUrl(): string {
+		return $this->urlGenerator->getAbsoluteURL(
+			$this->urlGenerator->imagePath(Application::APP_ID, 'app-dark.svg')
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getSupportedSearchProviderIds(): array {
+		if ($this->userId !== null) {
+			$ids = [];
+			// take the fallback value from the defaults
+			$searchEnabled = $this->config->getUserValue(
+				$this->userId,
+				Application::APP_ID,
+				'search_enabled',
+				$this->config->getAppValue(Application::APP_ID, 'default_enable_unified_search', '0')
+			);
+			if ($searchEnabled) {
+				$ids[] = 'openproject-search';
+			}
+			return $ids;
+		}
+		return ['openproject-search'];
+	}
+
+	/**
+	 * Parse a link to find a work package ID
+	 *
+	 * @param string $referenceText
+	 *
+	 * @return int|null
+	 */
+	private function getWorkPackageIdFromUrl(string $referenceText): ?int {
+		// example links
+		// https://community.openproject.org/projects/nextcloud-integration/work_packages/40070
+		$openProjectUrl = $this->config->getAppValue(Application::APP_ID, 'openproject_instance_url');
+		preg_match('/^' . preg_quote($openProjectUrl, '/') . '\/projects\/[^\/\?]+\/work_packages\/([0-9]+)/', $referenceText, $matches);
+		if (count($matches) > 1) {
+			return (int) $matches[1];
+		}
+
+		return null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function matchReference(string $referenceText): bool {
+		if ($this->userId !== null) {
+			$linkPreviewEnabled = $this->config->getUserValue($this->userId, Application::APP_ID, 'link_preview_enabled', '1') === '1';
+			if (!$linkPreviewEnabled) {
+				return false;
+			}
+		}
+		$adminLinkPreviewEnabled = $this->config->getAppValue(Application::APP_ID, 'link_preview_enabled', '1') === '1';
+		if (!$adminLinkPreviewEnabled) {
+			return false;
+		}
+
+		return $this->getWorkPackageIdFromUrl($referenceText) !== null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function resolveReference(string $referenceText): ?IReference {
+		if ($this->matchReference($referenceText)) {
+			$wpId = $this->getWorkPackageIdFromUrl($referenceText);
+			if ($wpId !== null) {
+				$wpInfo = $this->openProjectAPIService->getWorkPackageInfo($this->userId, $wpId);
+
+				$reference = new Reference($referenceText);
+				// this is used if your custom reference widget cannot be loaded (in mobile/desktop clients for example)
+				$reference->setTitle($wpInfo['title']);
+				$reference->setDescription($wpInfo['description']);
+				$reference->setImageUrl($wpInfo['imageUrl']);
+				// this is the data you will get in your custom reference widget
+				$reference->setRichObject(
+					self::RICH_OBJECT_TYPE,
+					$wpInfo
+				);
+				return $reference;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * We use the userId here because when connecting/disconnecting from the OpenProject account,
+	 * we want to invalidate all the user cache and this is only possible with the cache prefix
+	 * @inheritDoc
+	 */
+	public function getCachePrefix(string $referenceId): string {
+		return $this->userId ?? '';
+	}
+
+	/**
+	 * We don't use the userId here but rather a reference unique id
+	 * @inheritDoc
+	 */
+	public function getCacheKey(string $referenceId): ?string {
+		$wpId = $this->getWorkPackageIdFromUrl($referenceId);
+		return $wpId ?? $referenceId;
+	}
+
+	/**
+	 * @param string $userId
+	 * @return void
+	 */
+	public function invalidateUserCache(string $userId): void {
+		$this->referenceManager->invalidateCache($userId);
+	}
+}
---
+++ src/reference.js
@@ -0,0 +1,43 @@
+/**
+ * @copyright Copyright (c) 2023 Julien Veyssier <julien-nc@posteo.net>
+ *
+ * @author Julien Veyssier <julien-nc@posteo.net>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// this requires @nextcloud/vue >= 7.9.0
+import { registerWidget } from '@nextcloud/vue/dist/Components/NcRichText.js'
+
+// this is required for lazy loading
+__webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
+__webpack_public_path__ = OC.linkTo('integration_openproject', 'js/') // eslint-disable-line
+
+// this is where we associate our widget component with the richobjects that we return in the reference provider
+registerWidget('integration_openproject_work_package', async (el, { richObjectType, richObject, accessible }) => {
+	// here we lazy load the components so it does not slow down the initial page load
+	const { default: Vue } = await import(/* webpackChunkName: "reference-wp-lazy" */'vue')
+	const { default: WorkPackageReferenceWidget } = await import(/* webpackChunkName: "reference-wp-lazy" */'./views/WorkPackageReferenceWidget.vue')
+	Vue.mixin({ methods: { t, n } })
+	const Widget = Vue.extend(WorkPackageReferenceWidget)
+	new Widget({
+		propsData: {
+			richObjectType,
+			richObject,
+			accessible,
+		},
+	}).$mount(el)
+})
