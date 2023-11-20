33165eedc3844ea82d657110475632482d6c4a33
rossnet <rossnet@users.noreply.github.com>
2023-11-20 07:21:31 +0100

    Update integration_setup.sh
    Support subdirectory for temporary files in order to make use of a read only image root fs during K8s bootstrapping of the connection.
    
    Signed-off-by: rossnet <rossnet@users.noreply.github.com>

--- integration_setup.sh
+++ integration_setup.sh
@@ -1,7 +1,10 @@
+# SPDX-FileCopyrightText: 2023 Bundesministerium des Innern und für Heimat, PG ZenDiS "Projektgruppe für Aufbau ZenDiS"
+# SPDX-FileCopyrightText: 2023 Nextcloud GmbH
+# SPDX-License-Identifier: AGPL-3.0-only
 #!/usr/bin/env bash
 
 # This bash script is to set up the whole `openproject_integration` app integration
-# To run this script the Nextcloud and OpenProject instances must be running
+# To run this script the Nextcloud and OpenProject instances must be up and running
 
 # variables from environment
 # NEXTCLOUD_HOST=<nextcloud_host_url>
@@ -11,16 +14,13 @@
 # NC_ADMIN_USERNAME=<nextcloud_admin_username>
 # NC_ADMIN_PASSWORD=<nextcloud_admin_password>
 # OPENPROJECT_STORAGE_NAME=<openproject_filestorage_name> This variable is the name of the storage which keeps the oauth information in OpenProject required for integration.
-# SETUP_PROJECT_FOLDER= <true|false> If true set the integration is done with project folder setup and vice-versa
+# SETUP_PROJECT_FOLDER=<true|false> If true set the integration is done with project folder setup and vice-versa
+# INTEGRATION_SETUP_DEBUG=<true|false> If true script will output more details (set -x/-v) and keep the payload files
+# INTEGRATION_SETUP_TEMP_DIR=<pre-existing_diretory> The payloads sent to the APIs are written into temporary files by default ./temp, you can specify a custom directory that is expected to pre-exist e.g. when bootstrapping in a K8s job with ephemeral storage.
 
 set -e
 
-# Support for "Debug mode"
-if [[ $INTEGRATION_SETUP_DEBUG == "true"  ]] ; then
-  echo "Debug mode is enabled"
-  set -x
-  set -v
-fi
+INTEGRATION_SETUP_DEFAULT_TEMP_DIR=./temp
 
 # helper functions
 log_error() {
@@ -35,60 +35,72 @@ log_success() {
   echo -e "\e[32m$1\e[0m"
 }
 
+# Support for "Debug mode"
+if [[ $INTEGRATION_SETUP_DEBUG == "true"  ]]; then
+  log_info "Debug mode is enabled"
+  set -x
+  set -v
+fi
+
+if [[ $INTEGRATION_SETUP_TEMP_DIR == ""  ]]; then
+  log_info "Using default temp dir: ${INTEGRATION_SETUP_DEFAULT_TEMP_DIR}"
+  INTEGRATION_SETUP_TEMP_DIR=${INTEGRATION_SETUP_DEFAULT_TEMP_DIR}
+  mkdir -p ${INTEGRATION_SETUP_TEMP_DIR}
+else
+  log_info "Using ${INTEGRATION_SETUP_TEMP_DIR} as non-default temporary directory"
+  if [ ! -d "${INTEGRATION_SETUP_DEFAULT_TEMP_DIR}" ]; then
+    log_error "Expecting the non-default temporary directory to pre-exist"
+    exit 1
+  fi
+fi
+
 # if something goes wrong or an error occurs during the setup of the whole integration
 # we can delete the storage created in OpenProject, otherwise it would need to be deleted manually when the script is run the next time
 deleteOPStorageAndPrintErrorResponse() {
-  echo  "$1" | jq
-  log_error "Setup of the integration failed :( !!"
+  echo "$1" | jq
+  log_error "Setup of the integration failed!"
   delete_storage_response=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} \
                       ${OPENPROJECT_BASEURL_FOR_STORAGE}/${storage_id} \
                       -H 'accept: application/hal+json' \
                       -H 'Content-Type: application/json' \
                       -H 'X-Requested-With: XMLHttpRequest')
-  if [[ ${delete_storage_response} == 204 ]]
-    then
-      log_info "File storage name \"${OPENPROJECT_STORAGE_NAME}\" has been deleted from OpenProject !!"
-    else
-      log_info "Failed to delete file storage \"${OPENPROJECT_STORAGE_NAME}\" from OpenProject !!"
-    fi
+  if [[ ${delete_storage_response} == 204 ]]; then
+      log_info "File storage name \"${OPENPROJECT_STORAGE_NAME}\" has been deleted from OpenProject!"
+  else
+      log_info "Failed to delete file storage \"${OPENPROJECT_STORAGE_NAME}\" from OpenProject!"
+  fi
 }
 
-# making sure that jq is installed
-if ! command -v jq &> /dev/null
-then
-  log_error "jq is not installed"
-  log_info "sudo apt install -y jq (ubuntu) or brew install jq (mac)"
-  exit 1
-fi
 
 # These URLs are just to check if the Nextcloud and OpenProject instances have been started or not before running the script
 NEXTCLOUD_HOST_STATE=$(curl -s -X GET ${NEXTCLOUD_HOST}/status.php)
 NEXTCLOUD_HOST_INSTALLED_STATE=$(echo $NEXTCLOUD_HOST_STATE | jq -r ".installed")
 NEXTCLOUD_HOST_MAINTENANCE_STATE=$(echo $NEXTCLOUD_HOST_STATE | jq -r ".maintenance")
-OPENPROJECT_HOST_STATE=$(curl -s -X GET -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} \
-                          ${OPENPROJECT_HOST}/api/v3/configuration | jq -r "._type")
 OPENPROJECT_BASEURL_FOR_STORAGE=${OPENPROJECT_HOST}/api/v3/storages
 NC_INTEGRATION_BASE_URL=${NEXTCLOUD_HOST}/index.php/apps/integration_openproject
+openproject_host_state_response=$(curl -s -X GET -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWORD} ${OPENPROJECT_HOST}/api/v3/configuration)
 # these two data are set to "false" when the integration is done without project folder setup
 setup_project_folder=false
 setup_app_password=false
 
-
 # check if both instances are started or not
-if [[ ${OPENPROJECT_HOST_STATE} != "Configuration" ]]
-then
-  log_error "OpenProject host cannot be reached!!"
-  exit 1
+if [[ $(echo $openproject_host_state_response | jq -r "._type") != "Configuration" ]]; then
+  if [[ $(echo $openproject_host_state_response | jq -r ".errorIdentifier") == "urn:openproject-org:api:v3:errors:Unauthenticated" ]]; then
+    log_error "OpenProject authentication failed!"
+    exit 1
+  else
+    log_error "Unhandled error checking for OpenProject's availability, please checkout the request's response:"
+    log_error "${$openproject_host_state_response}"
+    exit 1
+  fi
 fi
-if [[ ${NEXTCLOUD_HOST_INSTALLED_STATE} != "true" || ${NEXTCLOUD_HOST_MAINTENANCE_STATE} != "false" ]]
-then
-  log_error "Nextcloud host cannot be reached or is in maintenance mode!!"
+if [[ ${NEXTCLOUD_HOST_INSTALLED_STATE} != "true" || ${NEXTCLOUD_HOST_MAINTENANCE_STATE} != "false" ]]; then
+  log_error "Nextcloud host cannot be reached or is in maintenance mode!"
   exit 1
 fi
 
 # we can set whether we want the integration with project folder or without it using environment variable 'SETUP_PROJECT_FOLDER'
-if [[ ${SETUP_PROJECT_FOLDER} == "true" ]]
-then
+if [[ ${SETUP_PROJECT_FOLDER} == "true" ]]; then
   # make an api request to get the status of the project folder setup
   project_folder_setup_response=$(curl -s -X GET -u${NC_ADMIN_USERNAME}:${NC_ADMIN_PASSWORD} \
                 ${NC_INTEGRATION_BASE_URL}/project-folder-status \
@@ -111,7 +123,7 @@ fi
 
 log_info "Creating file storage name \"${OPENPROJECT_STORAGE_NAME}\" in OpenProject ..."
 # api call to get openproject_client_id and openproject_client_secret
-cat >request_body_1_op_create_storage.json <<EOF
+cat >${INTEGRATION_SETUP_TEMP_DIR}/request_body_1_op_create_storage.json <<EOF
 {
   "name":"${OPENPROJECT_STORAGE_NAME}",
   "applicationPassword":"",
@@ -131,9 +143,9 @@ create_storage_response=$(curl -X POST -u${OP_ADMIN_USERNAME}:${OP_ADMIN_PASSWOR
   -H 'accept: application/hal+json' \
   -H 'Content-Type: application/json' \
   -H 'X-Requested-With: XMLHttpRequest' \
-  -d @request_body_1_op_create_storage.json
+  -d @${INTEGRATION_SETUP_TEMP_DIR}/request_body_1_op_create_storage.json
 )
-if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm request_body_1_op_create_storage.json; fi
+if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]]; then rm ${INTEGRATION_SETUP_TEMP_DIR}/request_body_1_op_create_storage.json; fi
 
 # check for errors
 response_type=$(echo $create_storage_response | jq -r "._type")
@@ -168,13 +180,13 @@ openproject_client_secret=$(echo $create_storage_response | jq -e '._embedded.oa
 if [[ ${storage_id} == null ]] || [[ ${openproject_client_id} == null ]] || [[ ${openproject_client_secret} == null ]]; then
   echo "${create_storage_response}" | jq
   log_error "Response does not contain storage_id (id) or openproject_client_id (clientId) or openproject_client_secret (clientSecret)"
-  log_error "Setup of the integration failed :( !!"
+  log_error "Setup of the integration failed!"
   exit 1
 fi
 log_info "success!"
 
 log_info "Setting up OpenProject integration in Nextcloud..."
-cat >request_body_2_nc_create_storage.json <<EOF
+cat >${INTEGRATION_SETUP_TEMP_DIR}/request_body_2_nc_create_storage.json <<EOF
 {
   "values":{
     "openproject_instance_url":"${OPENPROJECT_HOST}",
@@ -190,16 +202,15 @@ EOF
 # api call to set the  openproject_client_id and openproject_client_secret to Nextcloud and also get nextcloud_client_id and nextcloud_client_secret
 nextcloud_information_response=$(curl -s -X${setup_method} -u${NC_ADMIN_USERNAME}:${NC_ADMIN_PASSWORD} "${NC_INTEGRATION_BASE_URL}/setup" \
   -H 'Content-Type: application/json' \
-  -d @request_body_2_nc_create_storage.json
+  -d @${INTEGRATION_SETUP_TEMP_DIR}/request_body_2_nc_create_storage.json
 )
-if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm request_body_2_nc_create_storage.json; fi
+if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm ${INTEGRATION_SETUP_TEMP_DIR}/request_body_2_nc_create_storage.json; fi
 
 # required information from the above response
 nextcloud_client_id=$(echo $nextcloud_information_response | jq -e ".nextcloud_client_id")
 nextcloud_client_secret=$(echo $nextcloud_information_response | jq -e ".nextcloud_client_secret")
 
-if [[ ${SETUP_PROJECT_FOLDER} == "true" ]]
-then
+if [[ ${SETUP_PROJECT_FOLDER} == "true" ]]; then
   openproject_user_app_password=$(echo $nextcloud_information_response | jq -e ".openproject_user_app_password")
   if [ ${openproject_user_app_password} == null ]; then
     deleteOPStorageAndPrintErrorResponse "$nextcloud_information_response"
@@ -218,7 +229,7 @@ fi
 
 log_info "success!"
 
-cat >request_body_3_op_set_nc_oauth_details.json <<EOF
+cat >${INTEGRATION_SETUP_TEMP_DIR}/request_body_3_op_set_nc_oauth_details.json <<EOF
 {
   "clientId": ${nextcloud_client_id},
   "clientSecret": ${nextcloud_client_secret}
@@ -231,9 +242,9 @@ set_nextcloud_to_storage_response=$(curl -s -X POST -u${OP_ADMIN_USERNAME}:${OP_
                                   -H 'accept: application/hal+json' \
                                   -H 'Content-Type: application/json' \
                                   -H 'X-Requested-With: XMLHttpRequest' \
-                                  -d @request_body_3_op_set_nc_oauth_details.json
+                                  -d @${INTEGRATION_SETUP_TEMP_DIR}/request_body_3_op_set_nc_oauth_details.json
 )
-if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm request_body_3_op_set_nc_oauth_details.json; fi
+if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm ${INTEGRATION_SETUP_TEMP_DIR}/request_body_3_op_set_nc_oauth_details.json; fi
 
 # if there is no error from the last api call then the integration can be declared successful
 response_type=$(echo $set_nextcloud_to_storage_response | jq -r "._type")
@@ -247,7 +258,7 @@ log_info "success!"
 if [[ ${SETUP_PROJECT_FOLDER} == "true" ]]; then
 # save the application password to OpenProject
   log_info "Saving 'OpenProject' user application password to OpenProject...."
-  cat >request_body_4_op_set_project_folder_app_password.json <<EOF
+  cat >${INTEGRATION_SETUP_TEMP_DIR}/request_body_4_op_set_project_folder_app_password.json <<EOF
   {
     "applicationPassword": ${openproject_user_app_password}
   }
@@ -257,9 +268,9 @@ EOF
                                     -H 'accept: application/hal+json' \
                                     -H 'Content-Type: application/json' \
                                     -H 'X-Requested-With: XMLHttpRequest' \
-                                    -d @request_body_4_op_set_project_folder_app_password.json
+                                    -d @${INTEGRATION_SETUP_TEMP_DIR}/request_body_4_op_set_project_folder_app_password.json
     )
-    if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm request_body_4_op_set_project_folder_app_password.json; fi
+    if [[ $INTEGRATION_SETUP_DEBUG != "true"  ]] ; then rm ${INTEGRATION_SETUP_TEMP_DIR}/request_body_4_op_set_project_folder_app_password.json; fi
     app_password_response_type=$(echo $save_app_password_response | jq -r "._type")
     has_application_password=$(echo $save_app_password_response | jq -r ".hasApplicationPassword")
     if [[ ${app_password_response_type} == "Error" ]] || [[ ${has_application_password} == null ]]; then
