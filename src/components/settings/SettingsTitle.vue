8c56c3da9bb4c990590a46d9e22d0064e08682b9
sagargurung1001@gmail.com <sagargurung1001@gmail.com>
2024-03-25 09:40:52 +0545

    Adjust the documentation link according to review
    Signed-off-by: sagargurung1001@gmail.com <sagargurung1001@gmail.com>

--- src/components/settings/SettingsTitle.vue
+++ src/components/settings/SettingsTitle.vue
@@ -23,14 +23,6 @@ export default {
 			type: String,
 			required: true,
 		},
-		isConnectedWithOpenproject: {
-			type: Boolean,
-			required: false,
-		},
-		showIntegrationSetupLinkInformation: {
-			type: Boolean,
-			required: false,
-		},
 	},
 	computed: {
 		title() {
@@ -42,14 +34,14 @@ export default {
 			return t('integration_openproject', 'Visit our documentation for in-depth information on {htmlLink} integration.', { htmlLink }, null, { escape: false, sanitize: false })
 		},
 		getUserGuideDocumentationLinkText() {
-			const linkText = t('integration_openproject', 'Nextcloud integration user guide')
+			const linkText = t('integration_openproject', 'user guide')
 			const htmlLink = `<a class="link" href="https://www.openproject.org/docs/user-guide/file-management/nextcloud-integration/" target="_blank" title="${linkText}">${linkText}</a>`
-			return t('integration_openproject', 'Please go to our {htmlLink} to learn more about how to work with the OpenProject integration.', { htmlLink }, null, { escape: false, sanitize: false })
+			return t('integration_openproject', 'Learn how to get the most out of the OpenProject integration by visiting our {htmlLink}.', { htmlLink }, null, { escape: false, sanitize: false })
 		},
 		sanitizedHintText() {
-			if (this.isSetting === 'admin' && this.showIntegrationSetupLinkInformation) {
+			if (this.isSetting === 'admin') {
 				return dompurify.sanitize(this.getSetUpIntegrationDocumentationLinkText, { ADD_ATTR: ['target'] })
-			} else if (this.isSetting === 'personal' && this.isConnectedWithOpenproject) {
+			} else if (this.isSetting === 'personal') {
 				return dompurify.sanitize(this.getUserGuideDocumentationLinkText, { ADD_ATTR: ['target'] })
 			}
 			return ''
@@ -80,6 +72,7 @@ export default {
 
 .settings .link {
 	color: #1a67a3 !important;
+	font-style: normal;
 }
 
 </style>
