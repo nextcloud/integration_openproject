160403f8fa464cd24c913ba9813f12782f25ce91
sagargurung1001@gmail.com <sagargurung1001@gmail.com>
2024-06-19 16:10:20 +0545

    new vue bump
    Signed-off-by: sagargurung1001@gmail.com <sagargurung1001@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,30 @@
 const path = require('path')
 const rootDir = path.resolve(__dirname, '../../../')
 
+const ignorePatterns = [
+	'vue-material-design-icons',
+	'@nextcloud/vue',
+]
+
 module.exports = {
 	testMatch: ['**/tests/**/*.spec.{js,ts}'],
 	moduleNameMapper: {
-		'\\.(scss)$': '<rootDir>/tests/jest/stubs/empty.js',
-		'@nextcloud/l10n/gettext': require.resolve('@nextcloud/l10n/gettext'),
+		'\\.(css|scss)$': '<rootDir>/tests/jest/stubs/empty.js',
+		'@nextcloud/l10n/gettext': require.resolve('@nextcloud/l10n/gettext')
 	},
-	preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+	transform: {
+		// process `*.js` files with `babel-jest`
+		'^.+\\.c?js$': 'babel-jest',
+		'^.+\\.vue$': '@vue/vue2-jest'
+	},
 	collectCoverage: true,
+	testEnvironment: 'jest-environment-jsdom',
 	coverageProvider: 'v8',
 	collectCoverageFrom: ['./src/**'],
 	coverageDirectory: '<rootDir>/coverage/jest/',
 	coverageReporters: ['lcov', 'html', 'text'],
 	transformIgnorePatterns: [
-		'node_modules/(?!(vue-material-design-icons|@nextcloud/vue-select)/)',
+		'node_modules/(?!(' + ignorePatterns.join('|') + ')/)',
 	],
 	setupFiles: ["<rootDir>/tests/jest/global.mock.js"]
 }
