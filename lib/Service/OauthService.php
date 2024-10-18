db2a701bb258a37b5a3cbafbc09bb0a099cefbe1
Sagar Gurung <46086950+SagarGi@users.noreply.github.com>
2024-10-18 09:27:03 +0545

    Back port changes to get server version in `release/2.7` branch for server version compare (#720)
    * Use version array while getting server version for version compare
    
    Signed-off-by: Sagar <sagargurung1001@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Sagar <sagargurung1001@gmail.com>
    
    ---------
    
    Signed-off-by: Sagar <sagargurung1001@gmail.com>

--- lib/Service/OauthService.php
+++ lib/Service/OauthService.php
@@ -81,7 +81,7 @@ class OauthService {
 		$client->setName($name);
 		$client->setRedirectUri(sprintf($redirectUri, $clientId));
 		$secret = $this->secureRandom->generate(64, self::validChars);
-		$nextcloudVersion = OC_Util::getVersionString();
+		$nextcloudVersion = implode('.', OC_Util::getVersion());
 		$client->setSecret($this->hashOrEncryptSecretBasedOnNextcloudVersion($secret, $nextcloudVersion));
 		$client->setClientIdentifier($clientId);
 		$client = $this->clientMapper->insert($client);
