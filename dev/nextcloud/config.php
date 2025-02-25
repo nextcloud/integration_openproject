bdda14feafa10bd301d08323ce8afdb094161d93
Saw-jan <saw.jan.grg3e@gmail.com>
2025-02-25 14:20:29 +0545

    chore: fix php code style
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- dev/nextcloud/config.php
+++ dev/nextcloud/config.php
@@ -1,15 +1,16 @@
 <?php
+
 $CONFIG = [
-	'apps_paths' => 
-	array (
-        0 => 
-		array (
+	'apps_paths' =>
+	array(
+		0 =>
+		array(
 			'path' => '/var/www/html/apps',
 			'url' => '/apps',
 			'writable' => false,
 		),
 		1 =>
-		array (
+		array(
 			'path' => '/var/www/html/custom_apps',
 			'url' => '/custom_apps',
 			'writable' => true,
