d519ba4a9413f3bc549b8f36857faa63abeaef6e
Saw-jan <saw.jan.grg3e@gmail.com>
2025-02-25 13:15:01 +0545

    build: add NC compose setup to use specific server branch
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

---
+++ dev/nextcloud/hooks/before-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+echo "-----------------------------------------------------------"
+echo "[INFO] Installing CA certificates..."
+echo "-----------------------------------------------------------"
+
+STEP_CERTS_DIR="/step/certs"
+
+if [ -d "$STEP_CERTS_DIR" ]; then
+    rm -rf /etc/ssl/certs/Step_Root_CA.pem /usr/local/share/ca-certificates/Step_Root_CA.crt
+    echo "[INFO] Linking root CA certificate..."
+    cp "$STEP_CERTS_DIR"/root_ca.crt /usr/local/share/ca-certificates/Step_Root_CA.crt
+    update-ca-certificates
+fi
