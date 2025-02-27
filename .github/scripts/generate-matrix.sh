b5b534e006b5ec18a3a93557fc19d3d91b436212
Saw-jan <saw.jan.grg3e@gmail.com>
2025-02-27 16:37:09 +0545

    ci: add docs
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- .github/scripts/generate-matrix.sh
+++ .github/scripts/generate-matrix.sh
@@ -37,12 +37,34 @@ function getphpMinorVersionVersion() {
     echo "$1" | cut -d'.' -f2
 }
 
+MATRIX=""
+function addMatrix() {
+    local nextcloudVersion=$1
+    local phpVersion=$2
+    local phpVersionMajor=$3
+    local phpVersionMinor=$4
+    local database=$5
+    MATRIX="$MATRIX{\"nextcloudVersion\": \"$nextcloudVersion\", \"phpVersion\": \"$phpVersion\", \"phpVersionMajor\": \"$phpVersionMajor\", \"phpVersionMinor\": \"$phpVersionMinor\", \"database\": \"$database\"},"
+}
+
+# Defaults
 defaultPhpVersion="8.2"
-defaultphpMajorVersion=$(getphpMajorVersionVersion "$defaultPhpVersion")
-defaultphpMinorVersion=$(getphpMinorVersionVersion "$defaultPhpVersion")
+defaultPhpMajorVersion=$(getphpMajorVersionVersion "$defaultPhpVersion")
+defaultPhpMinorVersion=$(getphpMinorVersionVersion "$defaultPhpVersion")
 defaultDb="mysql"
 
-MATRIX=""
+# [NOTE]
+# This generates single job for the older nextcloud version
+#   with corresponding php version and mysql database.
+# And generates all combination for the latest nextcloud version
+#   with all php versions and mysql database
+# Example:
+#    nextcloudVersions="30 31 master" and phpVersions="8.1 8.2"
+#  Generates:
+#    - 30 8.2 mysql
+#    - 31 8.1 mysql
+#    - 31 8.2 mysql
+#    - master 8.3 mysql
 for ncVersion in $nextcloudVersions; do
     phpVersion=""
     phpVersionMajor=""
@@ -65,20 +87,20 @@ for ncVersion in $nextcloudVersions; do
     if [ -n "$phpVersion" ]; then
         phpVersionMajor=$(getphpMajorVersionVersion "$phpVersion")
         phpVersionMinor=$(getphpMinorVersionVersion "$phpVersion")
-        MATRIX="$MATRIX{\"nextcloudVersion\": \"$ncVersion\", \"phpVersion\": \"$phpVersion\", \"phpVersionMajor\": \"$phpVersionMajor\", \"phpVersionMinor\": \"$phpVersionMinor\", \"database\": \"$defaultDb\"},"
+        addMatrix "$ncVersion" "$phpVersion" "$phpVersionMajor" "$phpVersionMinor" "$defaultDb"
         continue
     fi
 
     # [INFO] Run all combination for the latest NC version
     for phpVersion in $phpVersions; do
-        MATRIX="$MATRIX{\"nextcloudVersion\": \"$ncVersion\", \"phpVersion\": \"$phpVersion\", \"phpVersionMajor\": \"$phpVersionMajor\", \"phpVersionMinor\": \"$phpVersionMinor\", \"database\": \"$defaultDb\"},"
+        addMatrix "$ncVersion" "$phpVersion" "$phpVersionMajor" "$phpVersionMinor" "$defaultDb"
     done
 
 done
 
 # add extra db matrix
 if [ -n "$extraDb" ]; then
-    MATRIX="$MATRIX{\"nextcloudVersion\": \"$latestSupportedNCVersion\", \"phpVersion\": \"$defaultPhpVersion\", \"phpVersionMajor\": \"$defaultphpMajorVersion\", \"phpVersionMinor\": \"$defaultphpMinorVersion\", \"database\": \"$extraDb\"},"
+    addMatrix "$latestSupportedNCVersion" "$defaultPhpVersion" "$defaultPhpMajorVersion" "$defaultPhpMinorVersion" "$extraDb"
 fi
 
 # remove last comma
