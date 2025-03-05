c536d41eb682dd1ec3264d50a7f51d858f92defe
Saw-jan <saw.jan.grg3e@gmail.com>
2025-03-05 10:35:48 +0545

    docs: add SPDX header
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- dev/nextcloud/config.php
+++ dev/nextcloud/config.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * SPDX-FileCopyrightText: 2025 Jankari Tech Pvt. Ltd.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 $CONFIG = [
 	'apps_paths' =>
 	array(
--- dev/nextcloud/hooks/before-install.sh
+++ dev/nextcloud/hooks/before-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2025 Jankari Tech Pvt. Ltd.
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 set -e
 
