3edb0d4f241cb742a7094d029cf016f3caf7da8a
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2025-06-19 12:34:09 +0545

    Merge pull request #845 from nextcloud/build/update-versions (#847)
    build: upgrade docker images

--- dev/nextcloud/hooks/after-install.sh
+++ dev/nextcloud/hooks/after-install.sh
@@ -4,10 +4,14 @@
 
 set -e
 
-OCC() {
-    # shellcheck disable=SC2068
-    sudo -E -u www-data php "$WEBROOT/occ" $@
-}
-
-OCC a:e integration_openproject
-OCC security:certificates:import /etc/ssl/certs/ca-certificates.crt
+occ a:e integration_openproject
+occ security:certificates:import /etc/ssl/certs/ca-certificates.crt
+# setup user_oidc app
+occ config:app:set --value=1 user_oidc store_login_token
+occ config:system:set user_oidc --type boolean --value="true" oidc_provider_bearer_validation
+occ user_oidc:provider:delete Keycloak
+occ user_oidc:provider Keycloak \
+    -c 'nextcloud' \
+    -s 'RGF6WCCOzm2bmnqgRelaIA5fp3XaMTIH' \
+    -d 'https://keycloak.local/realms/opnc/.well-known/openid-configuration' \
+    -o 'openid profile email api_v3'
