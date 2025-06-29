a1673edb97fc44581dc1b31a920b08343181dffd
nabim777 <nabinalemagar019@gmail.com>
2025-06-29 16:22:59 +0545

    Refactor: admin config validation for OAuth2 and OIDC auth
    Signed-off-by: nabim777 <nabinalemagar019@gmail.com>

--- lib/Service/OpenProjectAPIService.php
+++ lib/Service/OpenProjectAPIService.php
@@ -896,13 +896,7 @@ class OpenProjectAPIService {
 	public static function isAdminConfigOkForOauth2(IConfig $config):bool {
 		$clientId = $config->getAppValue(Application::APP_ID, 'openproject_client_id');
 		$clientSecret = $config->getAppValue(Application::APP_ID, 'openproject_client_secret');
-		$oauthInstanceUrl = $config->getAppValue(Application::APP_ID, 'openproject_instance_url');
-		$checkIfConfigIsSet = !!($clientId) && !!($clientSecret) && !!($oauthInstanceUrl);
-		if (!$checkIfConfigIsSet) {
-			return false;
-		} else {
-			return self::validateURL($oauthInstanceUrl);
-		}
+		return !(empty($clientId) || empty($clientSecret));
 	}
 
 	/**
@@ -914,14 +908,20 @@ class OpenProjectAPIService {
 	 */
 	public static function isAdminConfigOkForOIDCAuth(IConfig $config):bool {
 		$oidcProvider = $config->getAppValue(Application::APP_ID, 'oidc_provider');
+		$ssoProviderType = $config->getAppValue(Application::APP_ID, 'sso_provider_type');
 		$targetAudienceClientId = $config->getAppValue(Application::APP_ID, 'targeted_audience_client_id');
-		$oauthInstanceUrl = $config->getAppValue(Application::APP_ID, 'openproject_instance_url');
-		$checkIfConfigIsSet = !!($oidcProvider) && !!($targetAudienceClientId) && !!($oauthInstanceUrl);
-		if (!$checkIfConfigIsSet) {
+		$tokenExchange = $config->getAppValue(Application::APP_ID, 'token_exchange');
+
+		if (empty($ssoProviderType) || empty($oidcProvider)) {
 			return false;
-		} else {
-			return self::validateURL($oauthInstanceUrl);
 		}
+
+		// check for external sso without token exchange
+		if ($ssoProviderType === SettingsService::EXTERNAL_OIDC_PROVIDER_TYPE && $tokenExchange !== true) {
+			return true;
+		}
+
+		return !empty($targetAudienceClientId);
 	}
 
 
@@ -931,10 +931,17 @@ class OpenProjectAPIService {
 	 * @return bool
 	 */
 	public static function isAdminConfigOk(IConfig $config): bool {
+		$oauthInstanceUrl = $config->getAppValue(Application::APP_ID, 'openproject_instance_url');
 		$authMethod = $config->getAppValue(Application::APP_ID, 'authorization_method');
+		if (empty($oauthInstanceUrl) || !self::validateURL($oauthInstanceUrl)) {
+			return false;
+		}
+
 		if ($authMethod === self::AUTH_METHOD_OAUTH) {
 			return self::isAdminConfigOkForOauth2($config);
-		} elseif ($authMethod === self::AUTH_METHOD_OIDC) {
+		}
+
+		if ($authMethod === self::AUTH_METHOD_OIDC) {
 			return self::isAdminConfigOkForOIDCAuth($config);
 		}
 		return false;
