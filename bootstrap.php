6b38e61fd481dbd1b542d344aa84c5442bed67f7
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2026-02-13 15:07:16 +0545

    fix: do not analyze server files (#982)
    * fix: do not analyze server files
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * fix: add fallback server path
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    ---------
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- bootstrap.php
+++ bootstrap.php
@@ -9,26 +9,23 @@ define('PHPUNIT_RUN', 1);
 use Composer\Autoload\ClassLoader;
 
 include_once __DIR__.'/vendor/autoload.php';
-if (getenv('SERVER_PATH')) {
-	$serverPath = getenv('SERVER_PATH');
-} elseif (file_exists(__DIR__ . '/server')) {
-	$serverPath = __DIR__ . '/server';
-} else {
+$serverPath = __DIR__ . '/server';
+
+if (!file_exists($serverPath . '/lib/base.php')) {
 	$serverPath = __DIR__ . '/../..';
 }
+if (!file_exists($serverPath . '/lib/base.php')) {
+	throw new RuntimeException('Server path not found: ' . $serverPath);
+}
+
 include_once $serverPath.'/3rdparty/autoload.php';
 require_once $serverPath. '/lib/base.php';
 
 $classLoader = new ClassLoader();
 $classLoader->addPsr4("OCA\\OpenProject\\", __DIR__ . '/lib', true);
-$classLoader->addPsr4("OCA\\OpenProject\\Service\\", __DIR__ . '/lib/Service', true);
-$classLoader->addPsr4("OCA\\OpenProject\\Settings\\", __DIR__ . '/lib/Settings', true);
 $classLoader->addPsr4("OCP\\", $serverPath . '/lib/public', true);
 $classLoader->addPsr4("OC\\", $serverPath . '/lib/private', true);
-$classLoader->addPsr4("OCA\\Files\\Event\\", $serverPath . '/apps/files/lib/Event', true);
-$classLoader->addPsr4("OCA\\OpenProject\\AppInfo\\", __DIR__ . '/lib/AppInfo', true);
-$classLoader->addPsr4("OCA\\OpenProject\\Controller\\", __DIR__ . '/lib/Controller', true);
-$classLoader->addPsr4("OCA\\OpenProject\\Exception\\", __DIR__ . '/lib/Exception', true);
+$classLoader->addPsr4("OCA\\Files\\", $serverPath . '/apps/files/lib', true);
 $classLoader->register();
 
 set_include_path(get_include_path() . PATH_SEPARATOR . '/usr/share/php');
