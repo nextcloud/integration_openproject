7cde02fe2463b146db1fe836c3914b619ac5db76
nabim777 <nabinalemagar019@gmail.com>
2026-05-15 09:30:29 +0545

    ci: review addresses
    Signed-off-by: nabim777 <nabinalemagar019@gmail.com>

---
+++ .github/scripts/register-and-publish-app-to-self-hosted-appstore.sh
@@ -0,0 +1,74 @@
+# SPDX-FileCopyrightText: 2026 Jankari Tech Pvt. Ltd.
+# SPDX-License-Identifier: AGPL-3.0-only
+
+set -e
+
+# helper functions
+log_error() {
+  echo -e "\e[31m$1\e[0m"
+}
+
+log_info() {
+  echo -e "\e[37m$1\e[0m"
+}
+
+log_success() {
+  echo -e "\e[32m$1\e[0m"
+}
+
+if [[ -z "$APP_VERSION" ]]; then
+  log_error "Environment variables APP_VERSION is missing."
+  exit 1
+fi
+
+APPSTORE_BASE_URL="${APPSTORE_BASE_URL:-http://localhost:8000}"
+# public certificate of the integration app for testing
+CERTIFICATE="-----BEGIN CERTIFICATE-----\r\nMIIEEjCCAvoCAhF6MA0GCSqGSIb3DQEBCwUAMHsxCzAJBgNVBAYTAkRFMRswGQYD\r\nVQQIDBJCYWRlbi1XdWVydHRlbWJlcmcxFzAVBgNVBAoMDk5leHRjbG91ZCBHbWJI\r\nMTYwNAYDVQQDDC1OZXh0Y2xvdWQgQ29kZSBTaWduaW5nIEludGVybWVkaWF0ZSBB\r\ndXRob3JpdHkwHhcNMjEwMzE4MTMwMTExWhcNMzEwNjI0MTMwMTExWjAiMSAwHgYD\r\nVQQDDBdpbnRlZ3JhdGlvbl9vcGVucHJvamVjdDCCAiIwDQYJKoZIhvcNAQEBBQAD\r\nggIPADCCAgoCggIBALn0ohZShOzR6UJAuN4IErLD5jenUWr83XnKCouC0qeXH6FI\r\nTNGTyOy\/KbDDRIoL1L20xYRl5UKwTbDye10ItUBhNcv72pJ2rDOSJrL84fqMxf00\r\nWdd\/APXJfNNqtgh1QTq9vvim9YCEu7JdeIhZK9ea89RPn47iSj7YijY78mGBfyfm\r\nqpHRYX\/QZAQcwjO2lE9soWUaZlrqu3mxTI218zmaqqcma4x3QakfsZeXZhQSU7D1\r\n6iYG8wy8IaYueJM5OoRRziBXoIfPpwYpEj4RhV1WME9jGhutyrHYg3jAdfvzsFVG\r\ngSVUP2ey1sq3HGZGbzWMBFLDGqfet0lGBIB0HTna1Zvu3ZnuK2uV3MObCmBBbBSs\r\n\/s8hyQTqWEbY2aqVoTBN5lyogwfL6pgZJFvhmtg21oHxBBqqAeQ+TZmWD62WorsX\r\n4F6Ahh1VKkmr5LkVvr2CfME0M1mj9s9gSc7ekXk1oHabH+wwgJV2ZhyezhXgWKgL\r\nUahjSRzkKqp5mbh27sg1kLCx9QNyXxaz8rnAcazGB00JzQlUmXg76cJ0v\/M3qihz\r\nQR5oju\/iMiUYKtqec9LU6wfvmGOOvtl2OFOD3ff69FPS2Km8He4pFWkSqw4DGivE\r\nIJLlgqLGIkWm+uNyocANtYqib52AYwJ\/nFMF6nzOvM1LoxHyJlFmudZRju2jAgMB\r\nAAEwDQYJKoZIhvcNAQELBQADggEBAD8mQtw0p3oh9fyOuyTmalHxoG9rLiV0Q2mz\r\n1T0jonVYN7YqSxS\/yWIQnZQ98x2nU93Be4G9VaLT0NZvRjnem2zemSVvuwp11GeK\r\ne80gJTaJjh8n1Z+gD6GU4C+LjWeiR75sd6Jcqfp3bqL6FGvSzIk3QQOfWuC03aXa\r\nFRleNH6rkMV30sWnXyocatculf7ThHZQMN1c0KuQFrd\/alQh\/+EyjBleLozkeC6G\r\n9IlE9DGRK0NUSvy7W68I7cVhR2ToE8oApdOJ1Cd6TpTYMRtvI2lQ4F7vF++ym0Lw\r\nMIxSI44hNeixh8Yn9rcy\/LqOUgl0niB5hfAiauRwHcOY5wf1hKE=\r\n-----END CERTIFICATE-----"
+APP_ID="integration_openproject"
+
+registerApps() {
+  app_name=$1
+
+  register_app=$(curl -s -o /dev/null -w "%{http_code}" -X POST -uadmin:admin \
+    ${APPSTORE_BASE_URL}/api/v1/apps \
+    -H "Content-Type: application/json" \
+    -d "{
+    \"certificate\": \"${CERTIFICATE}\",
+    \"signature\": \"signature\"
+    }")
+  if [[ ${register_app} == 201 ]]; then
+    log_success "\"${app_name}\" has been registered successfully!"
+  elif [[ ${register_app} == 204 ]]; then
+    log_info "\"${app_name}\" has been updated!"
+  elif [[ ${register_app} == 400 ]]; then
+    log_error "\"${app_name}\" contains invalid characters, the signature!"
+    exit 1
+  else
+    log_error "Failed to register \"${app_name}\""
+    exit 1
+  fi
+}
+
+publishApps() {
+  app_name=$1
+  app_version=$2
+
+  register_app=$(curl -s -o /dev/null -w "%{http_code}" -X POST -uadmin:admin \
+    ${APPSTORE_BASE_URL}/api/v1/apps/releases \
+    -H "Content-Type: application/json" \
+    -d "{
+    \"download\":\"https://github.com/nextcloud/$APP_ID/releases/download/v$APP_VERSION/$APP_ID-$APP_VERSION.tar.gz\",
+    \"signature\": \"signature\"
+    }")
+  if [[ ${register_app} == 200 ]]; then
+    log_success "\"${app_name} ${app_version}\" has been updated successfully!"
+  elif [[ ${register_app} == 201 ]]; then
+    log_success "\"${app_name} ${app_version}\" has been published successfully!"
+  else
+    log_error "Failed to publish \"${app_name} ${app_version}\""
+    exit 1
+  fi
+}
+
+registerApps "$APP_ID"
+publishApps "$APP_ID" "$APP_VERSION"
