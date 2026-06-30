26f354867252d438bd3d4d8f52ab610a236fc799
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2026-06-30 14:24:06 +0545

    refactor: split oauth2 settings form (#1084)
    * refactor: split oauth2 settings form
    
    
    
    * test: fix AdminSettings unit tests
    
    
    
    * chore: add css classes
    
    
    
    * test: add vue unit tests for FormOAuthSettings
    
    
    
    * fix: get proper error message
    
    
    
    * test: fix AdminSettings unit tests
    
    
    
    * fix: add missing openproject storage link
    
    
    
    * chore: address reviews
    
    
    
    ---------
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -8,4 +8,5 @@ import { generateUrl } from '@nextcloud/router'
 export default {
 	validateOPInstance: generateUrl('/apps/integration_openproject/is-valid-op-instance'),
 	adminConfig: generateUrl('/apps/integration_openproject/admin-config'),
+	nextcloudOAuth: generateUrl('/apps/integration_openproject/nc-oauth'),
 }
