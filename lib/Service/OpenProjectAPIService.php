4bae69c643594479d757f18b0adc152578ab30c0
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2026-08-18 12:40:18 +0545

    fix: no encryption warning when the encryption is enabled (#1144)
    * fix: no encyrption warning when the encryption is enabled
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * chore: add changelog entry
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    * fix: no encyrption warning when the encryption is enabled
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>
    
    ---------
    
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- lib/Service/OpenProjectAPIService.php
+++ lib/Service/OpenProjectAPIService.php
@@ -1248,12 +1248,13 @@ class OpenProjectAPIService {
 	}
 
 	public function isServerSideEncryptionEnabled(): bool {
-		$isEncryptionForHomeStorageEnabled = $this->config->getAppValue('encryption', 'encryptHomeStorage', '0') === '1';
-		return (
-			$this->appManager->isInstalled('encryption') &&
-			$this->encryptionManager->isEnabled() &&
-			$isEncryptionForHomeStorageEnabled
-		);
+		$homeStorageEncrypted = false;
+		if ($this->appManager->isInstalled('encryption')) {
+			// home storage encryption is enabled by default
+			// when the default encryption module is enabled
+			$homeStorageEncrypted = $this->config->getAppValue('encryption', 'encryptHomeStorage', '1') === '1';
+		}
+		return $this->encryptionManager->isEnabled() && $homeStorageEncrypted;
 	}
 
 	/**
