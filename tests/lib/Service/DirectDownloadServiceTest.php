c306b1001cbfb4e40b95f30aafa3a080b5e07441
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2026-09-22 17:07:35 +0545

    test: fix DirectDownloadService phpunit test (#1182)
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- tests/lib/Service/DirectDownloadServiceTest.php
+++ tests/lib/Service/DirectDownloadServiceTest.php
@@ -8,97 +8,141 @@
 
 namespace OCA\OpenProject\Service;
 
-use OC\User\Session;
-use OCA\DAV\Controller\DirectController;
-use OCA\OpenProject\AppInfo\Application;
+use OC\Files\Node\File;
+use OCP\DB\IResult;
+use OCP\DB\QueryBuilder\IExpressionBuilder;
+use OCP\DB\QueryBuilder\IParameter;
+use OCP\DB\QueryBuilder\IQueryBuilder;
 use OCP\Files\Folder;
 use OCP\Files\IRootFolder;
-use OCP\IUserManager;
+use OCP\Files\IUserFolder;
+use OCP\IDBConnection;
 use PHPUnit\Framework\TestCase;
 
 class DirectDownloadServiceTest extends TestCase {
 	private const USER_ID = 'test';
-	private const USER_PASSWORD = 'T0T0T0T0T0T0T0';
-	/**
-	 * @var DirectDownloadService
-	 */
-	private $directDownloadService;
-	/**
-	 * @var Folder
-	 */
-	private $userFolder;
-	/**
-	 * @var DirectController
-	 */
-	private $directController;
 
-	public static function setUpBeforeClass(): void {
-		$app = new Application();
-		$c = $app->getContainer();
-
-		$userManager = $c->get(IUserManager::class);
-		$user = $userManager->get(self::USER_ID);
-		if ($user !== null) {
-			$user->delete();
+	/**
+	 * @return mixed
+	 */
+	public function getFolderMock(): mixed {
+		if (interface_exists(IUserFolder::class)) {
+			return $this->createMock(IUserFolder::class);
 		}
-
-		// create dummy user
-		$userManager->createUser(self::USER_ID, self::USER_PASSWORD);
-	}
-
-	protected function setUp(): void {
-		$app = new Application();
-		$c = $app->getContainer();
-
-		$userManager = $c->get(IUserManager::class);
-		$user = $userManager->get(self::USER_ID);
-		$userSession = $c->get(Session::class);
-		$userSession->setUser($user);
-
-		/** @var DirectDownloadService $directDownloadService */
-		$directDownloadService = $c->get(DirectDownloadService::class);
-		$this->directDownloadService = $directDownloadService;
-
-		/** @var IRootFolder $root */
-		$root = $c->get(IRootFolder::class);
-		$this->userFolder = $root->getUserFolder(self::USER_ID);
-
-		$this->directController = $c->get(DirectController::class);
-	}
-
-	public static function tearDownAfterClass(): void {
-		$app = new Application();
-		$c = $app->getContainer();
-		$userManager = $c->get(IUserManager::class);
-		$user = $userManager->get(self::USER_ID);
-		$user->delete();
-	}
-
-	protected function tearDown(): void {
+		return $this->createMock(Folder::class);
 	}
 
 	/**
-	 * @return void
-	 * @throws \OCP\AppFramework\OCS\OCSBadRequestException
-	 * @throws \OCP\AppFramework\OCS\OCSNotFoundException
-	 * @throws \OCP\Files\InvalidPathException
-	 * @throws \OCP\Files\NotFoundException
-	 * @throws \OCP\Files\NotPermittedException
-	 * @throws \OCP\Lock\LockedException
+	 * @param array $rows
+	 *
+	 * @return \Generator
 	 */
-	public function testGetDirectDownloadFile() {
-		// create a file
-		$originalFile = $this->userFolder->newFile('example.txt', 'dummy content');
+	public function dbTableGenerator($rows = []) {
+		foreach ($rows as $row) {
+			yield $row;
+		}
+	}
 
-		// create a direct download link and get its token
-		$response = $this->directController->getUrl($originalFile->getId());
-		$directLinkUrl = $response->getData()['url'];
-		preg_match('/.*\/([^\/]*)$/', $directLinkUrl, $matches);
-		$directLinkToken = $matches[1];
+	/**
+	 * @return array
+	 */
+	public function directDownloadDataProvider(): array {
+		return [
+			"existent file" => [[
+				'file_id' => 1,
+				'expiration' => time() + 3600,
+				'user_id' => self::USER_ID,
+			]],
+			"non-existent file" => [[]],
+		];
+	}
 
-		// get the file with our custom service
-		$serviceFile = $this->directDownloadService->getDirectDownloadFile($directLinkToken);
-		$this->assertSame($originalFile->getId(), $serviceFile->getId());
-		$this->assertSame($originalFile->getContent(), $serviceFile->getContent());
+	/**
+	 * @dataProvider directDownloadDataProvider
+	 *
+	 * @return void
+	 */
+	public function testGetDirectDownloadFile($dbRows) {
+		$token = '1234abcd';
+
+		$qExpBuilder = $this->createMock(IExpressionBuilder::class);
+		$qExpBuilder
+			->expects($this->once())
+			->method('eq')
+			->with('token', $this->isInstanceOf(IParameter::class));
+
+		$tableGenerator = $this->dbTableGenerator([$dbRows]);
+		$resultMock = $this->createMock(IResult::class);
+		$resultMock
+			->method('fetch')
+			->willReturnCallback(function () use ($tableGenerator) {
+				$row = $tableGenerator->current();
+				$tableGenerator->next();
+				return $row;
+			});
+		$resultMock
+			->expects($this->once())
+			->method('closeCursor');
+
+		$queryBuilderMock = $this->createMock(IQueryBuilder::class);
+		$queryBuilderMock
+			->expects($this->once())
+			->method('select')
+			->with('id', 'user_id', 'file_id', 'token', 'expiration')
+			->willReturnSelf();
+		$queryBuilderMock
+			->expects($this->once())
+			->method('from')
+			->with('directlink')
+			->willReturnSelf();
+		$queryBuilderMock
+			->expects($this->once())
+			->method('where')
+			->willReturnSelf();
+		$queryBuilderMock
+			->expects($this->once())
+			->method('expr')
+			->willReturn($qExpBuilder);
+		$queryBuilderMock
+			->expects($this->once())
+			->method('createNamedParameter')
+			->with($token, IQueryBuilder::PARAM_STR)
+			->willReturn($this->createMock(IParameter::class));
+		$queryBuilderMock
+			->expects($this->once())
+			->method('executeQuery')
+			->willReturn($resultMock);
+		$queryBuilderMock
+			->expects($this->once())
+			->method('resetQueryParts');
+
+		$dbMock = $this->createMock(IDBConnection::class);
+		$dbMock->method('getQueryBuilder')
+			->willReturn($queryBuilderMock);
+
+		$fileMock = $this->createMock(File::class);
+		$folderMock = $this->getFolderMock();
+		$folderMock
+			->method('getById')
+			->with(1)
+			->willReturn([$fileMock]);
+
+		$rootFolderMock = $this->createMock(IRootFolder::class);
+		$rootFolderMock
+			->method('getUserFolder')
+			->willReturn($folderMock);
+
+		$directDownloadService = new DirectDownloadService(
+			$dbMock,
+			$rootFolderMock,
+		);
+
+		$result = $directDownloadService->getDirectDownloadFile($token);
+
+		if (isset($dbRows) && !empty($dbRows)) {
+			$this->assertInstanceOf(File::class, $result);
+		} else {
+			$this->assertNull($result);
+		}
 	}
 }
